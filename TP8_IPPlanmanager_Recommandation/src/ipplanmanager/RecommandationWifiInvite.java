3be64a0f8f36b0f3468f4d03fc1486169c236fcd
WORLD TECH STORE <WORLD TECH STORE@DESKTOP-RP1PFK9>
2026-05-11 10:23:58 +0100

    Envoie du projet1POO 2026

---
+++ TP8_IPPlanmanager_Recommandation/src/ipplanmanager/RecommandationWifiInvite.java
@@ -0,0 +1,20 @@
+
+package ipplanmanager;
+
+/**
+ *
+ * @author doly
+ */
+public class RecommandationWifiInvite implements RegleRecommandation {
+    @Override
+    public Recommandation analyser(VLAN vlan) {
+        if (vlan.getNom().toUpperCase().contains("WIFI")) {
+            return new Recommandation(
+                    "Isolation du WiFi",
+                    "ÉLEVÉE",
+                    "Le VLAN " + vlan.getNom() + " doit être isolé des VLANs internes sensibles."
+            );
+        }
+        return null;
+    }
+}
